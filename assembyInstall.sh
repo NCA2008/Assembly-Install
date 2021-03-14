3aafa5bb74b8190a4d5d0a66dba19ac30ad18e26
NCA2008 <73907362+NCA2008@users.noreply.github.com>
2021-03-14 10:42:00 -0400

    Add files via upload

---
+++ assembyInstall.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -eux
+
+# Build.
+sudo apt-get build-dep binutils
+git clone git://sourceware.org/git/binutils-gdb.git
+cd binutils-gdb
+git checkout binutils-2_31
+./configure --target x86_64-elf --prefix "$(pwd)/install"
+make -j `nproc`
+make install
+
+# Test it out.
+cat <<'EOF' > hello.S
+.data
+    s:
+        .ascii "hello world\n"
+        len = . - s
+.text
+    .global _start
+    _start:
+        mov $4, %eax
+        mov $1, %ebx
+        mov $s, %ecx
+        mov $len, %edx
+        int $0x80
+        mov $1, %eax
+        mov $0, %ebx
+        int $0x80
+EOF
+./install/bin/x86_64-elf-as -o hello.o hello.S
+./install/bin/x86_64-elf-ld -o hello hello.o
+./hello
